fd617c0935b35f4c765985ddb6c61530513dcb35
osama <osama@osama_disktop.gju.edu.jo>
2018-03-04 11:39:30 +0200

    updating test11

--- p3.6/test11.py
+++ p3.6/test11.py
@@ -11,9 +11,16 @@ class Emp:
     def full_name(self):
         return '{} {}'.format(self.first, self.last)
 
+    @full_name.setter
+    def full_name(self, name):
+        first, last = name.split(' ')
+        self.first = first
+        self.last = last
+
 
 emp1 = Emp('john', 'dou')
 emp1.first = 'lucas'
+emp1.full_name = 'mark due'
 print(emp1.first)
 print(emp1.email)
 print(emp1.full_name)
