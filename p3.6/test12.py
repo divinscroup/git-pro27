4f2cc7d8d36aa358b0e8b528327aef52b9a6980c
osama khatib <osamakhatib@outlook.com>
2018-04-06 12:08:52 +0300

    updating

---
+++ p3.6/test12.py
@@ -0,0 +1,11 @@
+ninjas = ['ryu', 'crystal', 'yoshi', 'ken']
+
+for ninja in ninjas:
+    if ninja == 'yoshi':
+        print(f'{ninja} is black belt!')
+        break
+    else:
+        print(ninja)
+chars = ['a', 'b', 'c', 'd']
+for a in range(len(chars) - 1, -1, -1):
+    print(a, chars[a])
